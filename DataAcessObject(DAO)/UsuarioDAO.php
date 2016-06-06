6dddb5049c091591a1f4b4fa7a47d350ca5eb046
Higor Senna <higorr_jf@hotmail.com>
2016-06-05 21:13:54 -0300

    Modificado prepare na UsuarioDAO

--- DataAcessObject(DAO)/UsuarioDAO.php
+++ DataAcessObject(DAO)/UsuarioDAO.php
@@ -12,16 +12,11 @@ class UsuarioDAO
 
         $sql = "INSERT INTO usuarios (NM_USUARIO,EMAIL,CPF,TELEFONE,SENHA) values(?,?,?,?,?)";
         $stmt = $db->prepare($sql);
-        $stmt -> bindParam(1,$usuario->getNome());
-        $stmt -> bindParam(2,$usuario->getEmail());
-        $stmt -> bindParam(3,$usuario->getCPF());
-        $stmt -> bindParam(4,$usuario->getTelefone());
-        $stmt -> bindParam(5,$usuario->getSenha());
-        // $valoresObjeto = array($usuario->getNome(),$usuario->getEmail(),$usuario->getCPF(),
-        //   $usuario->getTelefone(),$usuario->getSenha());
 
+         $valoresObjeto = array($usuario->getNome(),$usuario->getEmail(),$usuario->getCPF(),
+          $usuario->getTelefone(),$usuario->getSenha());
 
-        if($stmt->execute()){
+        if($stmt->execute($valoresObjeto)){
             echo"            
             <script>
                 alert('Adicionado');
