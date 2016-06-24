fc5a3af21f5cef4cc088bd2c59bb6e0b09986126
Higor Senna <higorr_jf@hotmail.com>
2016-06-24 01:03:24 -0300

    Retirado botao em excesso na tela

--- pages/cadastroUsuario.php
+++ pages/cadastroUsuario.php
@@ -43,7 +43,7 @@
         </div>
     </div>
 
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-sm">Login no sistema</button>
+       
 
         <div class="modal fade bd-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
             <div class="row">
